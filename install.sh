a321ff6a97489139cf5e00e928e71b1e7084b8e4
James Borkowski <james.borkowski@corp.badoo.com>
2014-08-21 17:20:02 +0100

    blank line

--- install.sh
+++ install.sh
@@ -69,6 +69,7 @@ reportPathVars 	USER_PREFIX 			\
 echo "${COL_GREEN}Install Type:	${COL_BOLD_PURPLE}${INSTALL_TYPE}${COL_RESET}"
 preparePath		"$( expandVar "${INSTALL_TYPE}_PREFIX" )"
 preparePath		"$( expandVar "${INSTALL_TYPE}_VERSIONS_PREFIX" )"
+echo
 echo "Returning to previous state..."
 rm -r -f -v "$( expandVar "${INSTALL_TYPE}_PREFIX" )"
 rm -r -f -v "$( expandVar "${INSTALL_TYPE}_VERSIONS_PREFIX" )"
